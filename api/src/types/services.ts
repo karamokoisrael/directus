611991ae251cee7ce28e6f5e3bcecacaf6c8cf54
axel.karamoko <axel.karamoko@inexa-ci.com>
2023-03-20 10:24:06 +0000

    feat: adding types for services

--- api/src/types/services.ts
+++ api/src/types/services.ts
@@ -3,6 +3,8 @@ import { SchemaOverview } from '../types';
 import { Accountability } from '@directus/shared/types';
 import { Item, PrimaryKey } from './items';
 import { Query } from '@directus/shared/types';
+import { MailService, GraphQLService, ActivityService, AssetsService, AuthenticationService, CollectionsService, DashboardsService, FieldsService, FilesService, FoldersService, ImportService, ItemsService, MetaService, NotificationsService, PanelsService, PayloadService, PermissionsService, PresetsService, RelationsService, RevisionsService, RolesService, ServerService, SettingsService, SpecificationService, TFAService, UsersService, UtilsService, WebhooksService } from '../services';
+import { AuthorizationService } from '../services/authorization';
 
 export type AbstractServiceOptions = {
 	knex?: Knex;
@@ -26,4 +28,41 @@ export interface AbstractService {
 
 	deleteOne(key: PrimaryKey): Promise<PrimaryKey>;
 	deleteMany(keys: PrimaryKey[]): Promise<PrimaryKey[]>;
+}
+
+export interface Services {
+	MailService: typeof MailService;
+	GraphQLService: typeof GraphQLService;
+	ActivityService: typeof ActivityService;
+	AssetsService: typeof AssetsService;
+	AuthenticationService: typeof AuthenticationService;
+	AuthorizationService: typeof AuthorizationService;
+	CollectionsService: typeof CollectionsService;
+	DashboardsService: typeof DashboardsService;
+	FieldsService: typeof FieldsService;
+	FilesService: typeof FilesService;
+	// FlowsService: typeof FlowsService;
+	FoldersService: typeof FoldersService;
+	ImportService: typeof ImportService;
+	// ExportService: typeof ExportService;
+	ItemsService: typeof ItemsService;
+	MetaService: typeof MetaService;
+	NotificationsService: typeof NotificationsService;
+	// OperationsService: typeof OperationsService;
+	PanelsService: typeof PanelsService;
+	PayloadService: typeof PayloadService;
+	PermissionsService: typeof PermissionsService;
+	PresetsService: typeof PresetsService;
+	RelationsService: typeof RelationsService;
+	RevisionsService: typeof RevisionsService;
+	RolesService: typeof RolesService;
+	// SchemaService: typeof SchemaService;
+	ServerService: typeof ServerService;
+	SettingsService: typeof SettingsService;
+	// SharesService: typeof SharesService;
+	SpecificationService: typeof SpecificationService;
+	TFAService: typeof TFAService;
+	UsersService: typeof UsersService;
+	UtilsService: typeof UtilsService;
+	WebhooksService: typeof WebhooksService;
 }
